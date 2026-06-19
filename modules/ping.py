cc6907eea86bdbec9a23798f522af4c985b9fd59
Raj pomane <95400073+Rajpomane@users.noreply.github.com>
2026-06-19 19:33:22 +0530

    Refactor ping command with progress updates

--- modules/ping.py
+++ modules/ping.py
@@ -15,18 +15,36 @@
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 
-from pyrogram import Client, filters
-from pyrogram.types import Message
+@Client.on_message(
+    filters.command(["ping"], ".") & (filters.me | filters.user(SUDO_USER))
+)
+async def pingme(client: Client, message: Message):
+    uptime = await get_readable_time((time.time() - StartTime))
+    start = datetime.now()
+    xx = await message.reply_text("**0% ▒▒▒▒▒▒▒▒▒▒**")
+    try:
+       await message.delete()
+    except:
+       pass
+    await xx.edit("**20% ██▒▒▒▒▒▒▒▒ 🚩😈ᴘᴏɪsᴏɴ ᴏᴘ ʙᴏʟᴛᴇ🚩😈**")
+    await xx.edit("**40% ████▒▒▒▒▒▒ 🚩😈ᴊᴀɪ sʜʀᴇᴇ ʀᴀᴍ**")
+    await xx.edit("**60% ██████▒▒▒▒ 🚩😈ʀᴀᴅʜᴇ ᴋʀɪsʜɴᴀ**")
+    await xx.edit("**80% ████████▒▒ 🚩😈Hᴀʀ ʜᴀʀ ᴍᴀʜᴀᴅᴇᴠ**")
+    await xx.edit("**100% ████████▒▒ 🚩Jᴀɪ sʜʀᴇᴇ ʀᴀᴍ🚩**")
+    end = datetime.now()
+    duration = (end - start).microseconds / 100
+    await xx.edit(
+        f"🚩🕉️ **🔥🖤ᴘᴏɪsᴏɴ ᴛᴏxɪᴄ ᴅᴏ ᴅɪʟ ᴇᴋ ᴊᴀᴀɴ🔥🖤**\n"
+        f"❏ **🚩😈ᴘᴏɪsᴏɴ ʙᴏᴛ ғɪʀᴇ ᴏɴ😈🚩**\n"
+        f"├• **🚩🕉️** - `%sms`\n"
+        f"├• **🚩🕉️ -** `{uptime}` \n"
+        f"└• **🚩🕉️:** {client.me.mention}" % (duration)
+    )
 
-from utils import modules_help, prefix
 
-
-@Client.on_message(filters.command(["ping", "p"], prefix) & filters.me)
-async def ping(client: Client, message: Message):
-    latency = await client.ping()
-    await message.edit(f"<b>Pong! {latency}ms</b>")
-
-
-modules_help["ping"] = {
-    "ping": "Check ping to Telegram servers",
-}
+add_command_help(
+    "ping",
+    [
+        ["ping", "Check bot alive or not."],
+    ],
+)
